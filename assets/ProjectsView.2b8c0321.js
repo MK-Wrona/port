fd65bff9065a100e2872baf9d00ab089a643b48b
Mara Krukovskaya <krukovmargaery@yandex.ru>
2022-11-02 04:33:27 -0400

    test build 2

---
+++ assets/ProjectsView.2b8c0321.js
@@ -0,0 +1,1 @@
+import{h as e,j as t,z as a,P as r}from"./index.08942d48.js";const _={__name:"ProjectsView",setup(c){return(o,s)=>(e(),t("main",null,[a(r)]))}};export{_ as default};
--- assets/ProjectsView.c9bf5adf.js
+++
@@ -1,1 +0,0 @@
-import{h as e,j as t,z as a,P as r}from"./index.587b11b7.js";const _={__name:"ProjectsView",setup(c){return(o,s)=>(e(),t("main",null,[a(r)]))}};export{_ as default};
